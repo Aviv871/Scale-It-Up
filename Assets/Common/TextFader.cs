efd926c6a9eb8311bd7846384c618e6c9ddab4a0
Aviv <21aviv12@gmail.com>
2024-08-16 00:40:57 +0300

    First commit, empty project and .gitignore

---
+++ Assets/Common/TextFader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFader : MonoBehaviour
+{
+    private Color startColor;
+    private Color endColor;
+    private bool startFading = false;
+    private float timeCounter;
+
+    public float fadingSpeed = 0.05f;
+    public bool fadeIn = true;
+
+    void Start()
+    {
+        if (fadeIn)
+        {
+            endColor = gameObject.GetComponent<Text>().color;
+            startColor = endColor;
+            startColor.a = 0f;
+        }
+        else // fadeOut
+        {
+            startColor = gameObject.GetComponent<Text>().color;
+            endColor = startColor;
+            endColor.a = 0f;
+        }
+
+        fade();
+    }
+
+    void fade()
+    {
+        timeCounter = 0f;
+        startFading = true;
+    }
+
+    void Update()
+    {
+        if (startFading)
+        {
+            timeCounter += Time.deltaTime;
+            gameObject.GetComponent<Text>().color = Color.Lerp(startColor, endColor, fadingSpeed * timeCounter);
+        }
+    }
+}
---
+++ Assets/SplashScreen/Splash.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour 
+{
+	public float time = 3f;
+	private float timer;
+
+	private void Update() 
+	{
+		timer += Time.deltaTime;
+
+		if(timer > time)
+		{
+			GetComponent<Fader>().FadeTo("Menu");
+		}
+	}
+}
